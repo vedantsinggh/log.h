b23cc042fd9108c5bf57c61a066d435b6827cf03
vedantsinggh <vedantsinggh@gmail.com>
2024-03-15 07:25:28 +0530

    add examples

---
+++ examples.cpp
@@ -0,0 +1,17 @@
+// LOG_INPLEMENTATION must be defined to use log functions
+#define LOG_IMPLEMENTATION 
+#include "./Log.h"
+
+int main(){
+	// use Log(string) to simple log directly to terminal
+	Log("Hellow World!");
+
+	// use Log(CODE, string) with different CODE enums to display different kinds of colored log messages 
+	Log(SUCCESS ,"Test case passed!");
+	Log(INFO, "I inform you!");
+	Log(WARN ,"I warn you!");
+	Log(ERROR ,"An error occured!");
+	
+	// use ColorLog(COLOR, string) with COLOE enum to print different colers onto terminal
+	ColorLog(CYAN ,"This is colored text.");
+}
