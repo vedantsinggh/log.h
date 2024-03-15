0e9f01b07a614e3862b0cb80731e632287253a26
vedantsinggh <vedantsinggh@gmail.com>
2024-03-15 07:10:11 +0530

    Added leveled logging

--- Log.h
+++ Log.h
@@ -4,9 +4,40 @@
 #define LOG_H
 
 void Log(std::string  message);
+void Log(int level, std::string  message);
+
+enum LOG_LEVEL{
+	SUCCESS, 
+	INFO,
+    ERROR,
+    WARN,
+};
 #endif //LOG_H
 
 #ifdef LOG_IMPLEMENTATION
+
+void Log(int level, std::string  message){
+	// Color text format "\033[COLORm TEXT \033[m
+	if (level == SUCCESS)
+	{
+		std::cout<< "\033[32m" <<"[SUCCESS] " << "\033[m";
+	}
+	if (level == INFO)
+	{
+		std::cout<< "\033[36m" <<"[INFO]    " << "\033[m";
+	}
+	if (level == WARN)
+	{
+		std::cout<< "\033[33m" <<"[WARN]    " << "\033[m";
+	}
+	if (level == ERROR)
+	{
+		std::cout<< "\033[31m" <<"[ERROR]   " << "\033[m";
+	}
+	std::cout<<message<<std::endl;
+}
+
+
 void Log(std::string  message){
 	std::cout<<message<<std::endl;
 }
